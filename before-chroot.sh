e303e73ab0486535559b856a17c3156c337d2aad
wangjiezhe <wangjiezhe@gmail.com>
2020-04-22 01:35:05 +0800

    Use btrfs only on /dev/vda2
    See: https://wiki.archlinux.org/index.php/GRUB#GUID_Partition_Table_(GPT)_specific_instructions

--- before-chroot.sh
+++ before-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkfs.btrfs -A 10240 -m single -L arch /dev/vda
-mount -o compress=lzo /dev/vda /mnt
+mkfs.btrfs -m single -L arch -f /dev/vda2
+mount -o compress=lzo /dev/vda2 /mnt
 
 cd /mnt
 btrfs su cr @
@@ -14,15 +14,15 @@ btrfs su cr @tmp
 
 cd /
 umount /mnt
-mount -o compress=lzo,subvol=@ /dev/vda /mnt
+mount -o compress=lzo,subvol=@ /dev/vda2 /mnt
 
 mkdir -p /mnt/{boot,home,srv,var/{log,cache/pacman/pkg,tmp}}
-mount -o compress=lzo,subvol=@boot /dev/vda /mnt/boot
-mount -o compress=lzo,subvol=@home /dev/vda /mnt/home
-mount -o compress=lzo,subvol=@log /dev/vda /mnt/var/log
-mount -o compress=lzo,subvol=@pkg /dev/vda /mnt/var/cache/pacman/pkg
-mount -o compress=lzo,subvol=@srv /dev/vda /mnt/srv
-mount -o compress=lzo,subvol=@tmp /dev/vda /mnt/var/tmp
+mount -o compress=lzo,subvol=@boot /dev/vda2 /mnt/boot
+mount -o compress=lzo,subvol=@home /dev/vda2 /mnt/home
+mount -o compress=lzo,subvol=@log /dev/vda2 /mnt/var/log
+mount -o compress=lzo,subvol=@pkg /dev/vda2 /mnt/var/cache/pacman/pkg
+mount -o compress=lzo,subvol=@srv /dev/vda2 /mnt/srv
+mount -o compress=lzo,subvol=@tmp /dev/vda2 /mnt/var/tmp
 
 pacstrap /mnt base base-devel snapper vim openssh linux linux-firmware grub btrfs-progs
 
